7448dc9827ec13ee0dd4578f7fe6027b360ce8c1
fzingg <fredericzingg@protonmail.com>
2017-02-17 10:49:50 +0000

    demo

---
+++ app/views/components.rb
@@ -0,0 +1,19 @@
+require 'opal'
+
+
+
+require 'hyper-react'
+require 'react/react-source'
+require 'reactrb/auto-import'
+if React::IsomorphicHelpers.on_opal_client?
+  require 'opal-jquery'
+  require 'browser'
+  require 'browser/interval'
+  require 'browser/delay'
+  # add any additional requires that can ONLY run on client here
+end
+
+# require 'hyper-mesh'
+# require 'models'
+
+require_tree './components'
